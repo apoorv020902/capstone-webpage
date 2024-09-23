26bbf6fd93af695edc6a38633950c7c9b367138d
Apoorv <apoorv.r02@gmail.com>
2024-09-22 21:54:29 -0500

    added jira link

--- src/components/JiraLink.js
+++ src/components/JiraLink.js
@@ -4,7 +4,7 @@ const JiraLink = () => {
   return (
     <div className="jira-link">
       <p>We are using Jira to manage the project tasks and deliverables. You can view our Jira board by clicking the link below:</p>
-      <a href="https://your-jira-link.com" target="_blank" rel="noopener noreferrer" className="link-button">View Jira Board</a>
+      <a href="https://ttu-team-nk0sk0vi.atlassian.net/jira/software/projects/SCRUM/boards/1" target="_blank" rel="noopener noreferrer" className="link-button">View Jira Board</a>
     </div>
   );
 };
